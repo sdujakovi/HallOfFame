6d0954140a813e2d6ca37a1ab19624ed4a583661
Stanko Dujaković <61595425+sdujakovi@users.noreply.github.com>
2022-10-17 21:31:32 +0200

    Implement domain

---
+++ domain/src/main/java/com/example/domain/game/Game.kt
@@ -0,0 +1,10 @@
+package com.example.domain.game
+
+class Game {
+    var id: String = ""
+    var tier: String = ""
+    var name: String = ""
+    var thumbnail: String = ""
+    var description: String = ""
+    var criticScore: Double = 0.0
+}
